79f1d70c5bc2fd134866e8d2f8546f6b23e1ea7e
Your Name <you@example.com>
2021-06-04 00:03:53 +0300

    lab

---
+++ 3 задание.py
@@ -0,0 +1,10 @@
+print('Введите 4 числа')
+num1=float(input('1:'))
+num2=float(input('2:'))
+num3=float(input('3:'))
+num4=float(input('4:'))
+sum1=num1+num2
+sum2=num3+num4
+res=sum1/sum2
+print("%.2f"%res)
+
---
+++ из.py
@@ -0,0 +1,15 @@
+import math
+PRINT ('1 kg konfet = ')
+INPUT costA
+PRINT ('1 kg pieczienia = ');
+INPUT costB
+PRINT ('1 kg yablok = '')
+INPUT costC
+PRINT ('X = ')
+INPUT x
+PRINT ('Y = ')
+INPUT y
+PRINT ('Z = ')
+INPUT z
+PRINT ('ALL COST = ');
+PRINT ('costA * x + costB * y + costC * z'
